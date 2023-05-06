982ff1b44b255f8a948091f15ef6e9270921ef35
rosg150498 <rosg150498@gmail.com>
2023-05-06 22:51:05 +0000

    Add function to generate quiz answers, adding comments and add style to quiz answers container to style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const quizanswers = document.getElementsByClassName('quiz-answers');
+
 // General Knowledge Questions
 
 let questions = [];
@@ -17,7 +19,7 @@ function loadQuizQuestion() {
 // Function that will return the quiz questions and answers data
 
 function showQuizQuestion(data) {
- const quizElement = document.getElementById('quiz-question');
+   const quizElement = document.getElementById('quiz-question');
    quizElement.innerHTML = ''; // remove any existing questions
    
    data.forEach((item, index) => {
@@ -30,6 +32,26 @@ function showQuizQuestion(data) {
      questionText.setAttribute('class', 'quiz-question');
      questionElement.appendChild(questionText);
    
+     const answerLetters = ['A', 'B', 'C', 'D'];
+     const allAnswers = [...item.incorrect_answers, item.correct_answer];
+     allAnswers.sort(() => Math.random() - 0.5); // shuffle answers
+   
+     for (let i = 0; i < allAnswers.length; i++) {
+       const answerText = allAnswers[i];
+       const answerElement = document.createElement('p');
+   
+       answerElement.classList.add(`answer-${answerLetters[i]}`);
+       answerElement.innerHTML = answerText;
+   
+       answerElement.addEventListener('click', () => {
+         checkAnswer(answerText, item.correct_answer);
+       });
+   
+       if (index > 0) { // Only add answers to the second and subsequent questions
+         questionElement.appendChild(answerElement);
+       }
+     }
+   
      quizElement.appendChild(questionElement);
    });
    
@@ -37,15 +59,35 @@ function showQuizQuestion(data) {
    showQuestion();
  }
  
- function showQuestion() {
-   const allQuestions = document.querySelectorAll('.question');
-     allQuestions.forEach(question => {
-       question.style.display = 'none';
-     });
+function showQuestion() {
+    const allQuestions = document.querySelectorAll('.question');
+    allQuestions.forEach(question => {
+        question.style.display = 'none';
+    });
    
-     const currentQuestion = document.getElementById(`question-${questionIndex}`);
-     currentQuestion.style.display = 'block';
-   }
+    const currentQuestion = document.getElementById(`question-${questionIndex}`);
+    currentQuestion.style.display = 'block';
+    
+    const currentAnswers = questions[questionIndex].incorrect_answers.concat(questions[questionIndex].correct_answer);
+    generateAnswers(currentAnswers);
+}
+ 
+function generateAnswers(listofAnswers) {
+    const quizanswers = document.querySelector('.quiz-answers');
+    quizanswers.innerHTML = ''; // remove any existing answers
+
+    const answerLetters = ['A', 'B', 'C', 'D'];
+    listofAnswers.forEach((answerText, index) => {
+        const answerElement = document.createElement('p');
+        answerElement.classList.add(`answer-${answerLetters[index]}`);
+        answerElement.textContent = `${answerLetters[index]}. ${answerText}`;
+
+        answerElement.addEventListener('click', () => {
+            checkAnswer(answerText, questions[questionIndex].correct_answer);
+        });
+
+        quizanswers.appendChild(answerElement); 
+    });
+}
 
 loadQuizQuestion();
-
