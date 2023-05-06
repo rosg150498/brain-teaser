006462ecd6e12a2092ae26575baa3b1491832d98
rosg150498 <rosg150498@gmail.com>
2023-05-06 20:14:47 +0000

    Add function that add quiz questions to the the quiz question div to index.html and add quiz question and margin top style to style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,17 +16,33 @@ function loadQuizQuestion() {
 // Function that will return the quiz questions and answers data
 
 function showQuizQuestion(data) {
-    const something = data.map(item => {
-        return {
-            difficulty: item.difficulty,
-            question: item.question,
-            correctAnswer: item.correct_answer,
-            answers: [...item.incorrect_answers, item.correct_answer]
-        };
-    });
+  const something = data.map(item => {
+    return {
+      difficulty: item.difficulty,
+      question: item.question,
+      correctAnswer: item.correct_answer,
+      answers: [...item.incorrect_answers, item.correct_answer]
+    };
+  });
 
-    questions.push(...something);
-    console.log(questions);
+  questions.push(...something);
+  console.log(questions);
+
+  // Adding questions to the quiz container
+  const quizElement = document.getElementById('quiz-question');
+  questions.forEach((question, index) => {
+    const questionElement = document.createElement('div');
+    questionElement.classList.add('question');
+
+    const questionText = document.createElement('p');
+    questionText.textContent = `${index + 1}. ${question.question}`;
+    questionText.setAttribute('class', 'quiz-question');
+    questionElement.appendChild(questionText);
+
+    
+    quizElement.appendChild(questionElement);
+  });
 }
 
 loadQuizQuestion();
+
