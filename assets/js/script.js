ea135f97f22f3c14e44db13877357ee607ed1602
rosg150498 <rosg150498@gmail.com>
2023-05-06 19:38:55 +0000

    Add function that returns the quiz questions and answers data to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,24 @@ function loadQuizQuestion() {
     const APIUrl = `https://opentdb.com/api.php?amount=10&category=9&type=multiple`;
     fetch(APIUrl)
         .then(result => result.json())
-        .then(data => showQuestion(data.results));
+        .then(data => showQuizQuestion(data.results));
 }
+
+
+// Function that will return the quiz questions and answers data
+
+function showQuizQuestion(data) {
+    const something = data.map(item => {
+        return {
+            difficulty: item.difficulty,
+            question: item.question,
+            correctAnswer: item.correct_answer,
+            answers: [...item.incorrect_answers, item.correct_answer]
+        };
+    });
+
+    questions.push(...something);
+    console.log(questions);
+}
+
+loadQuizQuestion();
