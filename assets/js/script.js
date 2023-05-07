4654eaae466cf7ea95250f5440cab623d3b23a74
rosg150498 <rosg150498@gmail.com>
2023-05-07 00:08:04 +0000

    Add answer element event listener, add submit answer button to script.js and add start style to style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-const quizanswers = document.getElementsByClassName('quiz-answers');
+const quizanswers = document.querySelector('.quiz-answers');
 
 // General Knowledge Questions
 
 let questions = [];
 let questionIndex = 0;
+let userAnswer;
 
 
 // Function that will load the quiz questions from the Open tdb API
@@ -19,59 +20,59 @@ function loadQuizQuestion() {
 // Function that will return the quiz questions and answers data
 
 function showQuizQuestion(data) {
-   const quizElement = document.getElementById('quiz-question');
-   quizElement.innerHTML = ''; // remove any existing questions
-   
-   data.forEach((item, index) => {
-     const questionElement = document.createElement('div');
-     questionElement.setAttribute('id', `question-${index}`);
-     questionElement.classList.add('question', 'hidden');
-   
-     const questionText = document.createElement('p');
-     questionText.textContent = `${index + 1}. ${item.question.replaceAll(/&#039;/g, "'").replaceAll(/&quot;/g, '"').replaceAll(/&shy;/g, '').replaceAll(/&rdquo;/g, '"')}`;
-     questionText.setAttribute('class', 'quiz-question');
-     questionElement.appendChild(questionText);
-   
-     const answerLetters = ['A', 'B', 'C', 'D'];
-     const allAnswers = [...item.incorrect_answers, item.correct_answer];
-     allAnswers.sort(() => Math.random() - 0.5); // shuffle answers
-   
-     for (let i = 0; i < allAnswers.length; i++) {
-       const answerText = allAnswers[i];
-       const answerElement = document.createElement('p');
-   
-       answerElement.classList.add(`answer-${answerLetters[i]}`);
-       answerElement.innerHTML = answerText;
-   
-       answerElement.addEventListener('click', () => {
-         checkAnswer(answerText, item.correct_answer);
-       });
-   
-       if (index > 0) { // Only add answers to the second and subsequent questions
-         questionElement.appendChild(answerElement);
-       }
-     }
-   
-     quizElement.appendChild(questionElement);
-   });
-   
-   questions = data;
-   showQuestion();
- }
- 
+    const quizElement = document.getElementById('quiz-question');
+    quizElement.innerHTML = ''; // remove any existing questions
+
+    data.forEach((item, index) => {
+        const questionElement = document.createElement('div');
+        questionElement.setAttribute('id', `question-${index}`);
+        questionElement.classList.add('question', 'hidden');
+
+        const questionText = document.createElement('p');
+        questionText.textContent = `${index + 1}. ${item.question.replaceAll(/&#039;/g, "'").replaceAll(/&quot;/g, '"').replaceAll(/&shy;/g, '').replaceAll(/&rdquo;/g, '"')}`;
+        questionText.setAttribute('class', 'quiz-question');
+        questionElement.appendChild(questionText);
+
+        const answerLetters = ['A', 'B', 'C', 'D'];
+        const allAnswers = [...item.incorrect_answers, item.correct_answer];
+        allAnswers.sort(() => Math.random() - 0.5); // shuffle answers
+
+        for (let i = 0; i < allAnswers.length; i++) {
+            const answerText = allAnswers[i];
+            const answerElement = document.createElement('p');
+
+            answerElement.classList.add(`answer-${answerLetters[i]}`);
+            answerElement.innerHTML = answerText;
+
+            answerElement.addEventListener('click', () => {
+                userAnswer = answerText;
+            });
+
+            if (index > 0) { // Only add answers to the second and subsequent questions
+                quizanswers.appendChild(answerElement);
+            }
+        }
+
+        quizElement.appendChild(questionElement);
+    });
+
+    questions = data;
+    showQuestion();
+}
+
 function showQuestion() {
     const allQuestions = document.querySelectorAll('.question');
     allQuestions.forEach(question => {
         question.style.display = 'none';
     });
-   
+
     const currentQuestion = document.getElementById(`question-${questionIndex}`);
     currentQuestion.style.display = 'block';
-    
+
     const currentAnswers = questions[questionIndex].incorrect_answers.concat(questions[questionIndex].correct_answer);
     generateAnswers(currentAnswers);
 }
- 
+
 function generateAnswers(listofAnswers) {
     const quizanswers = document.querySelector('.quiz-answers');
     quizanswers.innerHTML = ''; // remove any existing answers
@@ -83,10 +84,23 @@ function generateAnswers(listofAnswers) {
         answerElement.textContent = `${answerLetters[index]}. ${answerText.replaceAll(/&rsquo;/g, "'").replaceAll(/&auml;/g, 'ä').replaceAll(/&aring;/g, 'å').replaceAll(/&ouml;/g, 'ö').replaceAll(/&oacute;/g, 'ó')}`;
 
         answerElement.addEventListener('click', () => {
-            checkAnswer(answerText, questions[questionIndex].correct_answer);
+            userAnswer = answerText;
         });
 
         quizanswers.appendChild(answerElement); 
+
+        const submitButton = document.createElement('button');
+        submitButton.setAttribute('class', 'submit-answer')
+        submitButton.textContent = 'Submit Answer';
+        submitButton.addEventListener('click', () => {
+            checkAnswer(userAnswer, questions[questionIndex].correct_answer);
+            questionIndex++;
+            if (questionIndex < questions.length) {
+                showQuestion();
+            } else {
+                // End of quiz
+            }
+        }); // fix here
     });
 }
 
